46b66ca0baa04a8681353e8678a1818ed64b1a66
krishna140404 <159816220+krishna140404@users.noreply.github.com>
2025-02-02 15:46:30 -0500

    Updated the performance.h file

--- b0a6ed4433ab23be380ef27d562bfd99-31764e24f724f7f69a99880c50eb4a2b834e698f/performance.h
+++ b0a6ed4433ab23be380ef27d562bfd99-31764e24f724f7f69a99880c50eb4a2b834e698f/performance.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef PERFORMANCE_H
+#define PERFORMANCE_H
+
+#include <chrono>
+
+class Performance {
+public:
+    void startTimer();
+    void stopTimer();
+    double getElapsedTime(); // In seconds
+    double calculateSpeed(size_t fileSize); // Speed in Mbps
+
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+};
+
+#endif // PERFORMANCE_H
