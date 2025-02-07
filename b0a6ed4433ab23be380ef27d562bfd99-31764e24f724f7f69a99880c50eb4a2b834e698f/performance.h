0de7205c507cd909a220db211e7a457f709a104d
krishna140404 <159816220+krishna140404@users.noreply.github.com>
2025-02-07 00:31:04 -0500

    remove some unneccesary things which was not meant to be there

--- b0a6ed4433ab23be380ef27d562bfd99-31764e24f724f7f69a99880c50eb4a2b834e698f/performance.h
+++ b0a6ed4433ab23be380ef27d562bfd99-31764e24f724f7f69a99880c50eb4a2b834e698f/performance.h
@@ -1,17 +1,14 @@
 #ifndef PERFORMANCE_H
 #define PERFORMANCE_H
 
-#include <chrono>
+#include <iostream>
+#include <ctime>
 
 class Performance {
 public:
-    void startTimer();
-    void stopTimer();
-    double getElapsedTime(); // In seconds
-    double calculateSpeed(size_t fileSize); // Speed in Mbps
-
-private:
-    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    static double getCurrentTime();
+    static void measureTransferSpeed(size_t fileSize, double startTime, double endTime);
 };
 
 #endif // PERFORMANCE_H
+
