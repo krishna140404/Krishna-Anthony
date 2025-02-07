5b9fd8f6011de1beaa95d787f84c2ff1a4e9fa9e
krishna140404 <159816220+krishna140404@users.noreply.github.com>
2025-02-06 23:19:14 -0500

    Delete b0a6ed4433ab23be380ef27d562bfd99-31764e24f724f7f69a99880c50eb4a2b834e698f/checksum.cpp

--- b0a6ed4433ab23be380ef27d562bfd99-31764e24f724f7f69a99880c50eb4a2b834e698f/checksum.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "checksum.h"
-#include <zlib.h> // CRC32 Implementation
-#pragma warning (disable:4996) 
-uint32_t calculateCRC32(const std::vector<uint8_t>& data) {
-    return crc32(0, data.data(), data.size());
-}
