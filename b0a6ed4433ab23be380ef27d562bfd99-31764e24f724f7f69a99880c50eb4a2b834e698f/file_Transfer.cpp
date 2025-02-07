3dc98dde33cb50d4514f679938dceecc2be03d35
krishna140404 <159816220+krishna140404@users.noreply.github.com>
2025-02-07 14:13:26 -0500

    Updated with correcting mistake  file_Transfer.cpp

--- b0a6ed4433ab23be380ef27d562bfd99-31764e24f724f7f69a99880c50eb4a2b834e698f/file_Transfer.cpp
+++ b0a6ed4433ab23be380ef27d562bfd99-31764e24f724f7f69a99880c50eb4a2b834e698f/file_Transfer.cpp
@@ -1,49 +1,47 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include "file_Transfer.h"
 
-class FileTransfer {
-public:
-    static bool readFile(const char* filename, char*& buffer, size_t& fileSize) {
-        std::ifstream file(filename, std::ios::binary);
-        if (!file) {
-            std::cerr << "Error: Unable to open file " << filename << std::endl;
-            return false;
+bool FileTransfer::readFile(const char* filename, char*& buffer, size_t& fileSize) {
+    std::ifstream file(filename, std::ios::binary);
+    if (!file) {
+        std::cerr << "Error: Unable to open file " << filename << std::endl;
+        return false;
+    }
+    file.seekg(0, std::ios::end);
+    fileSize = file.tellg();
+    file.seekg(0, std::ios::beg);
+    buffer = new char[fileSize];
+    file.read(buffer, fileSize);
+    file.close();
+    return true;
+}
+
+unsigned int FileTransfer::calculateCRC(const char* data, size_t length) {
+    unsigned int crc = 0xFFFFFFFF;
+    for (size_t i = 0; i < length; ++i) {
+        crc ^= static_cast<unsigned int>(data[i]);
+        for (int j = 0; j < 8; ++j) {
+            if (crc & 1)
+                crc = (crc >> 1) ^ 0xEDB88320;
+            else
+                crc >>= 1;
         }
-        file.seekg(0, std::ios::end);
-        fileSize = file.tellg();
-        file.seekg(0, std::ios::beg);
-        buffer = new char[fileSize];
-        file.read(buffer, fileSize);
-        file.close();
-        return true;
+    }
+    return crc ^ 0xFFFFFFFF;
+}
+
+void FileTransfer::sendFile(const char* filename) {
+    char* fileBuffer;
+    size_t fileSize;
+    if (!readFile(filename, fileBuffer, fileSize)) {
+        return;
     }
 
-    static unsigned int calculateCRC(const char* data, size_t length) {
-        unsigned int crc = 0xFFFFFFFF;
-        for (size_t i = 0; i < length; ++i) {
-            crc ^= static_cast<unsigned int>(data[i]);
-            for (int j = 0; j < 8; ++j) {
-                if (crc & 1)
-                    crc = (crc >> 1) ^ 0xEDB88320;
-                else
-                    crc >>= 1;
-            }
-        }
-        return crc ^ 0xFFFFFFFF;
-    }
-   
-    static void sendFile(const char* filename) {
-        char* fileBuffer;
-        size_t fileSize;
-        if (!readFile(filename, fileBuffer, fileSize)) {
-            return;
-        }
+    unsigned int crc = calculateCRC(fileBuffer, fileSize);
+    std::cout << "Sending file: " << filename << " (" << fileSize << " bytes)" << std::endl;
+    std::cout << "CRC Checksum: " << crc << std::endl;
 
-        unsigned int crc = calculateCRC(fileBuffer, fileSize);
-        std::cout << "Sending file: " << filename << " (" << fileSize << " bytes)" << std::endl;
-        std::cout << "CRC Checksum: " << crc << std::endl;
-
-        delete[] fileBuffer;
-    }
-};
+    delete[] fileBuffer;
+}
