ce3d4d98869f53a916c0a2893409b0b6d60f0f9d
krishna140404 <159816220+krishna140404@users.noreply.github.com>
2025-02-08 22:31:00 -0500

    added comments in file_Transfer.h

--- b0a6ed4433ab23be380ef27d562bfd99-31764e24f724f7f69a99880c50eb4a2b834e698f/file_Transfer.h
+++ b0a6ed4433ab23be380ef27d562bfd99-31764e24f724f7f69a99880c50eb4a2b834e698f/file_Transfer.h
@@ -5,12 +5,13 @@
 #include <fstream>
 #include <cstring>
 
+// defining the structure 
 class FileTransfer {
 public:
     static bool readFile(const char* filename, char*& buffer, size_t& fileSize);
     static unsigned int calculateCRC(const char* data, size_t length);
     static void sendFile(const char* filename);
-    static void requestFilenameClient(void);
 };
 
 #endif // FILE_TRANSFER_H
+
